20df7d5b1c40d30555f43ae1f0b4603fe7a66468
“yuyeah21” <yuyeah21@gmail.com>
2019-05-02 04:14:29 -0500

    モデル、コントローラ作成、ブートストラップ導入

---
+++ db/migrate/20190502081721_create_authors.rb
@@ -0,0 +1,11 @@
+class CreateAuthors < ActiveRecord::Migration[5.2]
+  def change
+    create_table :authors do |t|
+      t.string :name
+      t.string :description
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
