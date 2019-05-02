c1b7c07dfb7172e35d394000951782e18ea3153a
“yuyeah21” <yuyeah21@gmail.com>
2019-05-02 05:45:37 -0500

    登録します

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,13 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
   end
 
   def edit
@@ -18,5 +22,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :category)
   end
 end
