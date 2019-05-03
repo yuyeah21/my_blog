577dee95cccbc7ab97d4899a04e4f5e588b78241
“yuyeah21” <yuyeah21@gmail.com>
2019-05-02 21:59:11 -0500

    完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,12 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :updte, :destroy]
   def index
-    @posts = Post.all
-    @new_posts = Post.all
+    @posts = Post.order(created_at: :desc)
+    @new_posts = Post.order(created_at: :desc).limit(5)
     @author = Author.first
   end
 
   def show
-    @post = Post.find(params[:id])
-    
   end
 
   def new
@@ -21,19 +20,14 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
-    puts "---------------------"
-    p @post
   end
 
   def update
-    @post =Post.find(params[:id])
     @post.update(post_params)
     redirect_to @post
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
@@ -41,5 +35,8 @@ class PostsController < ApplicationController
   private
   def post_params
     params.require(:post).permit(:title, :body, :category)
+  end
+  def set_post
+    @post=Post.find(params[:id])
   end
 end
