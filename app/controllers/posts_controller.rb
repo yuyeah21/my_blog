b4d4cf1eb84934fa3b22b3635103fcd0b512ec42
“yuyeah21” <yuyeah21@gmail.com>
2019-05-02 22:34:48 -0500

    検索追加 

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :updte, :destroy]
   def index
-    @posts = Post.order(created_at: :desc)
+    @q = Post.order(created_at: :desc).ransack(params[:q])
+    @posts = @q.result(distinct: true)
     @new_posts = Post.order(created_at: :desc).limit(5)
     @author = Author.first
   end
